d359fc1f321c7a9bb1d729f70b6f9384e21241cf
Ashwin <ashwin.singh.087@gmail.com>
2021-06-24 16:23:28 -0400

    first commit - api info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    axios({
+    method: 'GET',
+    url: 'http://www.omdbapi.com/',
+    dataResponse: 'json',
+    params: {
+      apikey: '5e358330',
+      t: 'harry potter'
+    }}).then(response => {
+      console.log(response);
+    });
+
+      // axios({
+  //   method: 'GET',
+  //   url: 'https://api.themoviedb.org/3/search/movie',
+  //   dataResponse: 'json',
+  //   params: {
+  //     api_key: '9709355fc5ce17fa911605a13712678d',
+  //     language: 'en-US',
+  //     page: 1,
+  //     query: 'harry potter'
+  //   }}).then(response => {
+  //     console.log(response);
+  //   });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+
