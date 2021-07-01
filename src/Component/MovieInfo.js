339bb1be00d78f48ad19417d612384bd23928c80
k9paws <candogservices@gmail.com>
2021-06-30 21:54:49 -0400

    modal styling

--- src/Component/MovieInfo.js
+++ src/Component/MovieInfo.js
@@ -5,8 +5,8 @@ const MovieInfo = ({
   handleClose,
   director,
   cast,
-  youTube, 
-  handleAddToList
+  youTube,
+  handleAddToList,
   // displayAddList
 }) => {
   return (
@@ -14,22 +14,32 @@ const MovieInfo = ({
       {movieInfoDetail ? (
         <div className="modal">
           <div>
-            <div className="modalContent">
-              <h2>{movieInfoDetail.original_title}</h2>
-              <h2>{director}</h2>
-              {cast
-                ? cast.map((castMember) => {
-                    return <h3>{castMember.name}</h3>;
-                  })
-                : null}
-              <h2>{movieInfoDetail.tagline}</h2>
+            <div className="modalTextContent">
+              <h3>{movieInfoDetail.title}</h3>
+              <h4>
+                <strong>Directed by: </strong>
+                <span>{director}</span>
+              </h4>
+              <h4>
+                <strong>Cast:</strong>{" "}
+                {cast
+                  ? cast.map((castMember) => {
+                      return <span> {castMember.name} </span>;
+                    })
+                  : null}{" "}
+              </h4>
+              {/* <h2>{movieInfoDetail.tagline}</h2> */}
               <p>{movieInfoDetail.overview}</p>
-              <span>{movieInfoDetail.genres.map((genre) => {
-                return <p>{genre.name}</p>;
-              })}</span>
-              <span>{movieInfoDetail.runtime}</span>
-              <span>Hello</span>
-
+              {/* <p>
+                {movieInfoDetail.genres.map((genre) => {
+                  return <p>{genre.name}</p>;
+                })}
+              </p> */}
+              <h4>
+                <strong>Runtime:</strong>{" "}
+                <span>{movieInfoDetail.runtime} mins</span>
+              </h4>
+              {/* <span>Hello</span> */}
               {youTube ? (
                 <ReactPlayer
                   url={`https://www.youtube.com/watch?v=${youTube}`}
@@ -41,16 +51,20 @@ const MovieInfo = ({
               )}
             </div>
 
-            <div className="poster-image">
+            <div className="modalImageContent">
+              {/* <div className="poster-image"> */}
               <img
                 src={`https://image.tmdb.org/t/p/w500${movieInfoDetail.poster_path}`}
                 alt={`Poster for ${movieInfoDetail.original_title}`}
               />
+              {/* </div> */}
+
+              <div className="addButton">
+                <button onClick={handleAddToList}>Add to List</button>
+              </div>
             </div>
 
             <button onClick={handleClose}>X</button>
-
-            <button onClick={handleAddToList}>Add to List</button>
           </div>
         </div>
       ) : null}
